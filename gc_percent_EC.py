a264f779e9bbb79a61d8c39f07380522b5eaf5e3
blu21 <gobillylu@gmail.com>
2017-11-08 00:32:18 -0500

    2nd commit of GC percent HW with user inputed DNA sequence

--- gc_percent_EC.py
+++ gc_percent_EC.py
@@ -1,12 +1,17 @@
-DNAinput = input ('Enter your DNA sequence: ') #allows user to input DNA sequence as argument
+#Billy Lu, GC Percent HW with user input DNA sequence
+
+DNA = input ('Enter your DNA sequence (can be upper or lower case): ') #allows user to input DNA sequence as argument
+
+# changed the DNA sequence into all upper case letters
+DNAinput = DNA.upper()
 
 G_num = DNAinput.count("G") #Declare G_num as the variable for the number of Gs in the sequence
-g_num = DNAinput.count("g") #Declare g_num as the variable for the number of gs in the sequence
 C_num = DNAinput.count("C") #Declare C_num as the variable for the number of Cs in the sequence
-c_num = DNAinput.count("C") #Declare c_num as the variable for the number of cs in the sequence
+
 seq_len = len (DNAinput) #count the number of nucleotides in the sequence
 
-GC = (((G_num + C_num + c_num + g_num)/seq_len)*100) #Calculating the GC percentage in the sequence
+GC = (((G_num + C_num)/seq_len)*100) #Calculating the GC percentage in the sequence
 GC_round = round (GC, 2) #Rounded GC percentage to 2 decimal places
 
+print ("Your DNA sequence is ", DNAinput) #prints out the user DNA sequence in all capital letters
 print ("GC percent= ", GC_round, "%") #print GC percentage
